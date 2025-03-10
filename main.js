d36073f02bcbefc2082fedb88dc3d8bc0d03cd3d
Abdennour Hg <abdennourhg2003@gmail.com>
2025-03-10 21:01:45 +0100

    update

--- main.js
+++ main.js
@@ -73,5 +73,6 @@ selctVille.addEventListener("change", () => {
         console.log("Error.");
         console.log("Error.");
         console.log("Error.");
+        console.log("Error.");
     }
 });
